1958b864b8cc329de61dd8a89c39d60568ee2677
Joshua Yoon <j8yoon@ucsd.edu>
2025-09-04 23:45:19 -0700

    MAE143B hw 4

--- MAE143B HW4/HW4.m
+++
@@ -1,8 +0,0 @@
-Gs = RR_tf([1],[1 0 100])
-K = 255
-z1 = 10
-p1 = 10
-Ds = K*RR_tf([1 0 z1^2],[1 2*p1 p1^2])
-P = (1/0.01+K)/K
-figure (2), RR_step(P*Gs*Ds/(1+Gs*D))
-figure (1), RR_rlocus(Gs,Ds)
--- MAE143B HW4/HW4Part6.m
+++
@@ -1,4 +0,0 @@
-% It is semi-causal. This means the controller can be realized in real time 
-% on the MCU without requiring future information, and 
-% therefore the implementation is valid. This equation is dependent on
-% present and past and input and output values.
